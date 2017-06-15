b52548f9feb4ee39e18488b5f4122efeee93594d
Alex Gian <agian@pint.com>
2017-06-15 15:29:23 -0700

    LoopCaster

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # loopcaster
+When you need to print out something after `n` iterations.
+
+# Usage
+Define a caster w/ the max iteration, steps you want to print, and a callback.
+
+```
+const LoopCaster = require('loopcaster');
+
+let caster = new LoopCaster(128, 32, function(num, data){
+        console.log(`Casting ${num} | ${data}`);
+});
+
+let sum = 0;
+
+for( int i; i<128; i++){
+    sum += 2;
+    caster.chant(sum);
+}
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// LoopCaster
+// Triggers (casts) a callback at an interval (through chanting)
+class LoopCaster {
+  constructor(size, interval, cb){
+    this.size = size;
+    this.interval = interval; 
+    this.count = 0;
+    this.callback = cb;
+  }
+
+  /**
+   * chant
+   * increments count and trigger callback at an inteval
+   */
+  chant(data){    
+    this.count++;
+    if(this.count%this.interval == 0 || this.count == this.size){
+      this.callback(this.count, data);
+    } 
+  }
+}
+
+module.exports = LoopCaster;
