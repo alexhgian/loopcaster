6a570169b98a84ecb0237af2b6eb33dd09b0b363
Alex Gian <agian@pint.com>
2017-06-15 15:34:55 -0700

    improve readme, demo

--- sample/demo0.js
+++ sample/demo0.js
@@ -1,10 +1,3 @@
-# loopcaster
-When you need to print out something after `n` iterations.
-
-# Usage
-Define a caster w/ the max iteration, steps you want to print, and a callback.
-
-```
 const LoopCaster = require('loopcaster');
 
 let caster = new LoopCaster(128, 32, function(num, data){
@@ -17,4 +10,3 @@ for( var i = 0; i < 128; i++ ){
     sum += 2;
     caster.chant(sum);
 }
-```
